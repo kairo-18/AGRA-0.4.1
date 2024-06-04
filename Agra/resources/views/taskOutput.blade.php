9e13baffecb93b7a9e70e95190550f57d15c9813
kairo-18 <gladimirdungo@gmail.com>
2024-06-04 11:47:42 +0800

    changed instruction text-size

--- Agra/resources/views/taskOutput.blade.php
+++ Agra/resources/views/taskOutput.blade.php
@@ -160,7 +160,7 @@
 <div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-2xl max-h-full">
         <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <div class="relative bg-gray-200 rounded-lg shadow dark:bg-gray-700">
             <!-- Modal header -->
             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                 <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
@@ -175,7 +175,7 @@
             </div>
             <!-- Modal body -->
             <div class="p-4 md:p-5 space-y-4">
-                <div class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                <div class="text-xl leading-relaxed text-black dark:text-gray-400">
                     {!! $task->TaskInstruction!!}
                 </div>
             </div>
