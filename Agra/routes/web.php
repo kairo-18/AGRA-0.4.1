ceadbb3754f495eda73027904cb9a3f80e1d815c
JahMinZawa <minniezawaa.11@gmail.com>
2024-11-22 07:36:12 +0800

    test

--- Agra/routes/web.php
+++ Agra/routes/web.php
@@ -913,7 +913,7 @@ Route::get('/userAnalytics', function () {
             $performance['overall_performance'] = $overallPerformance;
 
             // Check if overall performance is below 45
-            if ($overallPerformance < 80) {
+            if ($overallPerformance < 90) {
                 $badperformancelessons[] = ['lesson_id' => $lessonId, 'performance' => $overallPerformance]; // Push lesson ID and performance to badperformancelessons array
             }
         }
